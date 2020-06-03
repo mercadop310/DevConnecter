aad1e115a83321cc7901370918d5b6ac0b31ca11
Peter Mercado <peter@Peters-MBP-10.lan>
2020-06-03 14:58:09 -0700

    finished routes for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,27 +20,87 @@ router.post(
     }
 
     try {
-        //find the user
-        const user = await User.findById(req.user.id).select('-password');
+      //find the user
+      const user = await User.findById(req.user.id).select('-password');
 
-        //create the new post with the information from the client
-        const newPost = new Post ({
-          text: req.body.text,
-          name: user.name,
-          avatar: user.avatar,
-          user: req.user.id,
-        });
+      //create the new post with the information from the client
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
 
-        //save the post in db and send it back towards client
-        const post = await newPost.save()
-        res.json(post)
+      //save the post in db and send it back towards client
+      const post = await newPost.save();
+      res.json(post);
     } catch (error) {
-        console.error(error.message)
-        res.status(500).send('Server Error')
+      console.error(error.message);
+      res.status(500).send('Server Error');
     }
-
-   
   }
 );
+
+// @route   Get api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    //retrieve all posts and sort them from latest to oldest
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   Get api/posts/:id
+// @desc    Get individual post by id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    //retrieve post by id
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Get all posts
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    //retrieve all posts and sort them from latest to oldest
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+    //Check if user is valid
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    //remove the post and notify client it was removed
+    await post.remove();
+    res.json({ msg: 'Post removed' });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
