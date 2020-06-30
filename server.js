d415938767cd0b1fa08c8555fa4f78d975abd58e
Peter Mercado <peter@Peters-MBP.lan>
2020-06-30 14:25:24 -0700

    preparing to deploy to heroku

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB();
 //Init Middleware (bodyPaster)
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
@@ -27,6 +25,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+const PORT = process.env.PORT || 5000;
 
 //initiate server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
